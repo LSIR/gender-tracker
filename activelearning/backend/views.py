8787478182b4f0898be4f30972c30e146f27fbc6
Niels Jens Poulsen <poulsen@tsf-460-wpa-0-191.epfl.ch>
2019-10-23 11:37:07 +0200

    Reorganised methods into a helper file

--- activelearning/backend/views.py
+++ activelearning/backend/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-import xml.etree.ElementTree as ET
+from .helpers import load_article, parse_xml
 import json
 
 def hello(request):
@@ -27,43 +27,3 @@ def submitTags(request):
             return HttpResponse('Failiure. JSON parse failed.')
     return HttpResponse('Failiure. Not a POST request.')
 
-
-# As I'm not sure where to put helper functions yet (my guess is that I'll write a helper.py file),
-# I'll write them in here for now
-
-def load_article():
-    """
-    :return: an article whose quotes we want to label.
-    Right now: loads the only xml-formatted article I have, and returns it as an ElementTree.
-    Future: Probably load a random article (or a carefully selected one), parse it and return
-    it as an ElementTree. How we choose each article to parse is the important part.
-    """
-    article_path = '../data/article01.xml'
-    return ET.parse(article_path)
-
-def parse_xml(xml_file):
-    """
-    :param xml_file: the ElementTree we want to parse into a JSON object
-    :return: the ElementTree as a JSON object. 
-    """
-    root = xml_file.getroot()
-    full_text = get_element_text(root)
-    returned_text = full_text[:45] +  ' ...'
-    split_words = returned_text.split(' ')
-    return {'sentence': split_words}
-
-def get_element_text(element):
-    """
-    Takes as input an ElementTree Element, and returns all text contained between its
-    two XML labels as a string.
-    :param element: the element whose contained text we want
-    :return: all text contained between "element"s two tags.
-    """
-    # Text as list of strings
-    ls = list(element.itertext())
-    # Concatenate
-    text = ''.join(ls)
-    # Clean text
-    text = ''.join(ls).replace('\n', '')
-    text = ' '.join(text.split())
-    return text

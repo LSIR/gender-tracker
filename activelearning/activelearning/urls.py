f7ccf08df5288885bee073ad4ded8f481b64e46a
Niels Jens Poulsen <poulsen@tsf-460-wpa-5-231.epfl.ch>
2020-01-31 16:52:49 +0100

    Added sentry to dockerfile

--- activelearning/activelearning/urls.py
+++ activelearning/activelearning/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+import backend
 
 urlpatterns = [
     path('admin/', admin.site.urls),
